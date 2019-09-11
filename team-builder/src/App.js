18f372877cabdbe12d57daf7dcabe82db016c1a6
jenny obryant <jennyobryant@protonmail.com>
2019-09-10 18:16:04 -0600

    2

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,48 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'; //1. import useState
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [members, setMembers] = useState([]); 
-  return (
+  const [members, setMembers] = useState([{
+    name: "jenny", 
+    email: "jennyobryant@protonmail.com",
+    role: "UI developer"
+  },
+  {
+    name: "jacob",
+    email:"jacobobryant@protonmai.com",
+    role: "backend developer"
+  }
+]); //2. give state variable
+  return ( //3. render the list of team members
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {members.map(member =>
+    <div>
+      <div>{member.name}</div>
+      <div>{member.email}</div>
+      <div>{member.role}</div>
+    </div>
+
+    )}
+
+{
+
+    //  <header className="App-header">
+     //   <img src={logo} className="App-logo" alt="logo" />
+      //  <p>
+      //    Edit <code>src/App.js</code> and save to reload.
+      //  </p>
+       // <a
+         // className="App-link"
+         // href="https://reactjs.org"
+         // target="_blank"
+         // rel="noopener noreferrer"
+       // >
+         // Learn React
+       // </a>
+     // </header>
+}
     </div>
   );
 }
